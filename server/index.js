2d0a168d38fb338d2ed44c9bd7c9531584cf52ee
Lucas Tao <tao63@purdue.edu>
2018-04-07 16:42:38 -0400

    Basic index.js file using express, body-parser, and firebase frameworks

---
+++ server/index.js
@@ -0,0 +1,87 @@
+var express = require('express');
+var body_parser = require('body-parser');
+var firebase = require('firebase')
+
+var app = express();
+app.use(body_parser.json())
+
+// Constants used for verifying JSON subsmission by users
+const username = "username";
+const password = "password";
+const email = "email";
+const newUsername = "newUsername";
+const newPassword = "newPassword";
+const newEmail = "newEmail";
+const host = "localhost";
+
+// Database constants
+const db_username = "username";
+const db_password = "password";
+const db_name = "database";
+const db_table = "accounts";
+
+const port = 3000;
+
+
+app.get('/', function(request, response) {
+  response.send("GET request\n");
+});
+
+// Called when a POST request is made to /registerAccount
+app.post('/registerAccount', function(request, response) {
+
+});
+
+// Called when a POST request is made to /login
+app.post('/login', function(request, response) {
+
+});
+
+// Called when a POST request is made to /deleteAccount
+app.post('/deleteAccount', function(request, response) {
+
+});
+
+// Called when a POST request is made to /changePassword
+app.post('/changePassword', function(request, response) {
+
+});
+
+// Called when a POST request is made to /changeEmail
+app.post('/changeEmail', function(request, response) {
+});
+
+// Called when a POST request is made to /forgotPassword
+app.post('/forgotPassword', function(request, response) {
+});
+
+// Helper function that registers a user if username and email does not already exist
+function register(u, p, e, response) {
+}
+
+// Helper function that verifies user has an account and logs them in
+function login(u, p, response) {
+}
+
+// Helper function that deletes an account
+function deleteAccount(u, p, e, response) {
+}
+
+// Helper function that changes the password of an account
+function changePassword(u, p, e, n, response) {
+}
+
+// Helper function that changes the email of an account
+function changeEmail(u, p, e, n, response) {
+}
+
+// Helper function for forgotten password
+function forgotPassword(u, e, response) {
+}
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('Listen error!', err);
+  }
+  console.log(`Server listening on port ${port}`);
+});
